f565d7d7814f63fbb501561f9d686cdd7d49c7c3
Loic Viens <loicviens@fas.harvard.edu>
2017-08-15 18:33:10 -0400

    Update KiKnet2sac.m

--- Codes/KiKnet2sac.m
+++ Codes/KiKnet2sac.m
@@ -1,15 +1,39 @@
-% L. VIENS 25/11/2017
+% L. VIENS 08/15/2017
 % Function to convert KiK-net ASCII files to sac files (acceleration
 % waveforms in gal)
 
-%  Input: input_file: path and file name
-% Output: output_file: path and name of the sac file
+% Input: FILENAME: path and filename
+
+%	KiKnet2sac(FILENAME) creates a Seismic Analysis Code (SAC) file called FILENAME.sac from the Kik-net ASCII file. 
+%   KiKnet2sac(FILENAME,OUTPUT_FILE) creates a Seismic Analysis Code (SAC)
+%   file called OUTPUTFILE from the Kik-net data (in ASCII format).
 
 % Requirements: import_KiKnet_header and matlab2sac functions
 
-function KiKnet2sac(input_file,output_file)
+function KiKnet2sac(FILENAME, varargin)
+
+if nargin < 1
+	error('Not enough input argument.')
+end
+
+if ~exist(FILENAME,'file')
+    disp(['!!!!!!!!!!!!!!!!' FILENAME ' does not exist !!!!!!!!!!!!!!!! '])
+    return
+end
+
+if nargin == 2
+    output_file = varargin{1};
+    if ~ischar(output_file)
+       disp(['Output file is not a character vector.'])
+       return
+    end
+else    
+    output_file = [FILENAME '.sac'];
+    disp(['The output file is: ' output_file])
+end
+
 %% Import header
-header = import_KiKnet_header(input_file);
+header = import_KiKnet_header(FILENAME);
 time1 = strsplit(header{1},' ');
 lat_event  = strsplit(header{2},' ');
 lon_event = strsplit(header{3},' ');
@@ -34,7 +58,7 @@ sca =  strsplit(header{14},' ');
 scale = strsplit(sca{3},'(gal)/');
 
 %% Import data
-[x1,x2,x3,x4,x5,x6,x7,x8] = import_KiKnet_data(input_file);
+[x1,x2,x3,x4,x5,x6,x7,x8] = import_KiKnet_data(FILENAME);
 x=[x1,x2,x3,x4,x5,x6,x7,x8];
 k=1;
 for j=1:length(x1)
@@ -52,7 +76,3 @@ matlab2sac(h,output_file,'DELTA', delta, 'EVLA', str2num(lat_event{2}), 'EVLO',s
     'KSTNM',(Sta_name{end}) , ...
     'NZYEAR',str2num(date{1}),'NZJDAY',dy,'NZHOUR' ,str2num(hr{1}) ,'NZMIN', str2num(hr{2}), 'NZSEC',str2num(hr{3}) ,...
     'KCMPNM',cmp{2});
-
-
-
-
